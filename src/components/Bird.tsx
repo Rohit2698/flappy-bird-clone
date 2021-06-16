5023b6f35430cf0ab2b89bb33f03a3ed6d895e0f
Rohit Thakur <rohit.thakur2698@gmail.com>
2021-06-17 00:10:37 +0530

    Added logic for obstacle crash.

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import '../App.css';
 type BirdProps = {
     birdFlightPos: number;
+    birdStartPosition: number;
 }
 
-const Bird: React.FC<BirdProps> = ({ birdFlightPos }) => {
+const Bird: React.FC<BirdProps> = ({ birdFlightPos, birdStartPosition }) => {
     return (
         <div
             className={"bird"}
             style={{
                 position: 'absolute',
                 top: 0 + birdFlightPos,
-                left: 500,
+                left: birdStartPosition + 40,
             }}
         />
     )
