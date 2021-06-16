58a2abc9db46439730eac7895a4ae07f3f0845cc
Rohit Thakur <rohit.thakur2698@gmail.com>
2021-06-17 00:10:50 +0530

    Added game end view.

---
+++ src/components/GameEndView.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const GameEndView = () => {
+
+    return (
+        <div style={{
+            position: 'absolute',
+            top: "50%",
+            zIndex: 1
+        }}>
+            <h1>Thank you for playing.... Press Enter to restart</h1>
+        </div>
+    )
+}
+
+export default GameEndView;
